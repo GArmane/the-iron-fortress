1f95bf625f9937ad42e98b6f118cee490df7a032
Giovanni Armane <giovanniarmane@gmail.com>
2020-10-18 17:05:59 -0300

    feat(actors/player/player.tscn): add idle, running and jumping animation

--- src/actors/player/player.gd
+++ src/actors/player/player.gd
@@ -9,16 +9,23 @@ func _physics_process(_delta):
 	motion.y += gravity
 	
 	if Input.is_action_pressed("ui_right"):
+		if is_on_floor():
+			$AnimationPlayer.play("walking")
 		$Sprite.flip_h = false
 		motion.x = 200
 	elif Input.is_action_pressed("ui_left"):
+		if is_on_floor():
+			$AnimationPlayer.play("walking")
 		$Sprite.flip_h = true
 		motion.x = -200
 	else:
+		if is_on_floor():
+			$AnimationPlayer.play("idle")
 		motion.x = 0
 
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
+			$AnimationPlayer.play("jumping")
 			motion.y -= 300
 
 	motion = move_and_slide(motion, UP)
