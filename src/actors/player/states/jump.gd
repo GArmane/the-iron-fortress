9b6781a14aa998404e7096ea296cef706d0cfaf8
Giovanni Armane <giovanniarmane@gmail.com>
2020-11-27 16:47:29 -0300

    refactor(actors/player): use general _set_motion function for movement

--- src/actors/player/states/jump.gd
+++ src/actors/player/states/jump.gd
@@ -3,7 +3,6 @@ extends "base_motion.gd"
 
 func enter() -> void:
 	player.play_animation("jump")
-	player.motion.x = 0
 	player.motion.y -= player.jump_height
 
 
@@ -11,3 +10,6 @@ func update(_delta: float) -> void:
 	if _is_falling():
 		emit_signal("finished", "fall")
 		return
+
+	var input_direction = _get_input_direction()
+	_set_motion(input_direction)
