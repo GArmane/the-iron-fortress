0db98c07e366ba1196ca50c9cd833826fde21b12
Giovanni Armane <giovanniarmane@gmail.com>
2020-11-29 22:58:41 -0300

    refactor(components/player_state_machine): make resources preload

--- src/components/player_state_machine/player_state_machine.gd
+++ src/components/player_state_machine/player_state_machine.gd
@@ -8,8 +8,8 @@ onready var player = owner
 # Callbacks
 func _ready() -> void:
 	_states = {
-		"fall": load("res://src/components/player_state_machine/states/fall.gd").new(player),
-		"idle": load("res://src/components/player_state_machine/states/idle.gd").new(player),
-		"jump": load("res://src/components/player_state_machine/states/jump.gd").new(player),
-		"walk": load("res://src/components/player_state_machine/states/walk.gd").new(player),
+		"fall": preload("res://src/components/player_state_machine/states/fall.gd").new(player),
+		"idle": preload("res://src/components/player_state_machine/states/idle.gd").new(player),
+		"jump": preload("res://src/components/player_state_machine/states/jump.gd").new(player),
+		"walk": preload("res://src/components/player_state_machine/states/walk.gd").new(player),
 	}
